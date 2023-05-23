68c00772b769f4db57b02bb69d4a9d3bd2d62cf1
Jiachengxu0723 <jiacheng.xu@synsense.ai>
2023-05-23 10:49:45 +0800

    20230512

--- practice/spider_homework.py
+++
@@ -1,8 +0,0 @@
-
-import json,requests
-
-keyword = '戴口罩'
-url = f'https://image.baidu.com/search/acjson?tn=resultjson_com&ipn=rj&queryWord={keyword}&ie=utf-8&oe=utf-8&word={keyword}'
-
-res = requests.get(url)
-print(res.text)
--- pythonProject/Excel.py
+++ pythonProject/Excel.py
@@ -5,11 +5,10 @@ import time
 import os,xlwt
 import pandas as pd
 
-
 '''
 读取Excel文件，输出指定的Sheet中指定的行和列
-
 '''
+
 def process_Excel(txt_path):
     #加载一个已存在的txt文件
     txt = load_workbook(txt_path)
@@ -24,10 +23,8 @@ def process_Excel(txt_path):
     print(lists[1][1].value)
     return
 
-
 '''
 根据要求批量操作Excel文件
-
 '''
 def updata_excel(excel_path):
     filename = os.listdir(excel_path)
@@ -47,7 +44,6 @@ def index():
     process_Excel(txt_path)
     for i in tqdm(range(1),desc='excel生成中',ncols=60):
         time.sleep(0.1)
-
 
 if __name__ == '__main__':
     txt_path = ('/home/synsense/下载/1.xlsx')
